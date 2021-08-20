a26b08fd888ef77d5b16b5af1f52a466b7b511e6
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-08-20 21:25:36 +0700

    38. Splitting Components Into Multiple Components (css)

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,11 @@
+import "./ExpenseDate.css"
+
 function ExpenseDate(props) {
   return (
-    <div>
-      <div>{props.month}</div>
-      <div>{props.year}</div>
-      <div>{props.day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{props.month}</div>
+      <div className="expense-date__year">{props.year}</div>
+      <div className="expense-date__day">{props.day}</div>
     </div>
   );
 }
