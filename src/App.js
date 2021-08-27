7319f2824d18be665c766661f8566e3759cbb3d2
Ajisri <ajis.riyananta@velintech.com>
2021-08-27 21:34:02 +0700

    39 fix

--- src/App.js
+++ src/App.js
@@ -31,9 +31,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <div className="expenses">
-        <Expenses items={expenses}></Expenses>
-      </div>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
