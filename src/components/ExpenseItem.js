bc865aa95b4a97e393da619cdf4699be21dca6ba
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-08-29 20:29:04 +0700

    mau mulai state

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,13 +9,25 @@ function ExpenseItem(props) {
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
+  let title = props.title; // "Toilet Paper"
+
+  const eventHandler = () => {
+    let newTitle = "Radio"; // newTitle = "Radio", title = "Toilet Paper"
+
+    title = newTitle; // newTitle = "Radio", title = "Radio"
+    // mengupdate komponen ini setelah ada perubahan variabel "title"
+    // dengan kata lain, mengupdate state komponen ini dari state awalnya
+    // menjadi state dengan "title" baru
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate month={month} day={day} year={year}></ExpenseDate>
       <div className="expense-item__description">
-        <ExpenseTitle title={props.title}></ExpenseTitle>
+        <ExpenseTitle title={title}></ExpenseTitle>
         <ExpenseAmount amount={props.amount}></ExpenseAmount>
       </div>
+      <button onClick={eventHandler}>Change Title</button>
     </Card>
   );
 }
