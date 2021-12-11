ba2dd88149341f14e4c2ec10915efb30a7b22dc5
Ajisri <ajis.riyananta@velintech.com>
2021-12-12 05:36:08 +0700

    video 62

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,24 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import ExpenseTitle from "./ExpenseTitle.js";
 import ExpenseAmount from "./ExpenseAmount.js";
-import Card from "./Card.js";
+import Card from "../UI/Card.js";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
-  let title = props.title; // "Toilet Paper"
-
   const eventHandler = () => {
-    let newTitle = "Radio"; // newTitle = "Radio", title = "Toilet Paper"
-
-    title = newTitle; // newTitle = "Radio", title = "Radio"
-    // mengupdate komponen ini setelah ada perubahan variabel "title"
-    // dengan kata lain, mengupdate state komponen ini dari state awalnya
-    // menjadi state dengan "title" baru
+    let newTitle = "Radio"; // newTitle = "Radio", title = "Toilet Paper". newTitle = "Radio", title = "Radio"
+    // // mengupdate komponen ini setelah ada perubahan variabel "title"
+    // // dengan kata lain, mengupdate state komponen ini dari state awalnya
+    // // menjadi state dengan "title" baru
+    // setTitle(newTitle);
+    setTitle(newTitle);
   };
 
   return (
